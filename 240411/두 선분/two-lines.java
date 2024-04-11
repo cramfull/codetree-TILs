52c1ad2e6d7d4a685168bcf46ea8f0bf8fc30990
cramfull <130737411+cramfull@users.noreply.github.com>
2024-04-11 13:25:07 +0900

    Title: 두 선분, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240411/두 선분/two-lines.java
+++ 240411/두 선분/two-lines.java
@@ -12,11 +12,12 @@ public class Main {
 
         boolean ff = false;
 
-        if(x3<=x2){
+        if((x1<x4)||(x2<x3)){
             ff = true;
         }
         if(ff){
             System.out.println("intersecting");
+
         }else{
             System.out.println("nonintersecting");
         }
